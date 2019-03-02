0cf9ad1312d79b8a3094c383f2a0d7b08d8f3dec
Ignacio Navarro <3074688+pirita@users.noreply.github.com>
2019-03-02 20:06:00 +0100

    Fix retrieve route

--- modules/client/src/main/scala/higherkindness/compendium/CompendiumClient.scala
+++ modules/client/src/main/scala/higherkindness/compendium/CompendiumClient.scala
@@ -17,12 +17,13 @@
 package higherkindness.compendium
 
 import cats.effect.Sync
+import cats.free.Free
+import cats.implicits._
 import hammock._
-import hammock.asynchttpclient.AsyncHttpClientInterpreter
 import hammock.circe.implicits._
 import higherkindness.compendium.http.Encoders._
 import higherkindness.compendium.http.Decoders._
-import higherkindness.compendium.models.{ClientConfig, Protocol}
+import higherkindness.compendium.models._
 
 trait CompendiumClient[F[_]] {
 
@@ -31,7 +32,7 @@ trait CompendiumClient[F[_]] {
    * @param protocol a protocol
    * @return the identifier of the protocol
    */
-  def storeProtocol(protocol: Protocol): F[String]
+  def storeProtocol(protocol: Protocol, identifier: String): F[Unit]
 
   /** Retrieve a Protocol by its id
    *
@@ -43,24 +44,48 @@ trait CompendiumClient[F[_]] {
 
 object CompendiumClient {
 
-  def impl[F[_]: Sync: AsyncHttpClientInterpreter](
-      clientConfig: ClientConfig): CompendiumClient[F] = {
+  def apply[F[_]](implicit F: CompendiumClient[F]): CompendiumClient[F] = F
+
+  implicit def impl[F[_]: Sync: InterpTrans](
+      implicit clientConfig: ClientConfig): CompendiumClient[F] = {
 
     val baseUrl: String = s"https://${clientConfig.http.host}:${clientConfig.http.port}"
 
     new CompendiumClient[F] {
 
-      override def storeProtocol(protocol: Protocol): F[String] =
+      override def storeProtocol(protocol: Protocol, identifier: String): F[Unit] =
         Hammock
-          .request(Method.POST, uri"$baseUrl/protocol/", Map(), Some(protocol))
-          .as[String]
+          .request(Method.POST, uri"$baseUrl/v0/protocol/$identifier", Map(), Some(protocol))
+          .as[Unit]
           .exec[F]
 
-      override def recoverProtocol(identifier: String): F[Option[Protocol]] =
-        Hammock
-          .request(Method.GET, uri"$baseUrl/protocol/", Map())
-          .as[Option[Protocol]]
+      override def recoverProtocol(identifier: String): F[Option[Protocol]] = {
+        val request: Free[HttpF, HttpResponse] = Hammock
+          .request(Method.GET, uri"$baseUrl/v0/protocol/$identifier", Map())
+
+        for {
+          status <- request.map(_.status).exec[F]
+          out <- status match {
+            case Status.OK         => request.as[Protocol].map(Some(_)).exec[F]
+            case Status.NotFound   => Sync[F].pure(None)
+            case Status.BadRequest => asError(request, new SchemaError(_))
+            case Status.Conflict   => asError(request, new ProtocolAlreadyExists(_))
+            case Status.InternalServerError =>
+              Sync[F].raiseError(new UnknownError(s"Error in compendium server"))
+            case _ =>
+              Sync[F].raiseError(new UnknownError(s"Unknown error with status code $status"))
+          }
+        } yield out
+      }
+
+      private def asError(
+          request: Free[HttpF, HttpResponse],
+          error: String => Exception): F[Option[Protocol]] =
+        request
+          .as[String]
           .exec[F]
+          .flatMap(str => Sync[F].raiseError(error(str)))
+
     }
   }
 }
