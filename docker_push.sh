2fcd014b47ea23b15314c54ae610bd3f835bd731
tzimisce012 <daniel.ochoa@47deg.com>
2019-05-24 17:05:05 +0200

    Fix name of repository

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "$1" | docker login -u "$2" --password-stdin
 export tag=$(cut -d'=' -f2  version.sbt | sed -e 's/^[ \t]*//;s/"//g')
-docker push $1/compendium:${tag}
-docker push $1/compendium:latest
+docker push $2/compendium:${tag}
+docker push $2/compendium:latest
