667b4d229fc38e6233299829713270b5dbca4bb3
tzimisce012 <daniel.ochoa@47deg.com>
2019-05-24 16:28:10 +0200

    Publish latest tag too

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USER" --password-stdin
+echo "$1" | docker login -u "$2" --password-stdin
 export tag=$(cut -d'=' -f2  version.sbt | sed -e 's/^[ \t]*//;s/"//g')
-docker push $DOCKER_USER/compendium:${tag}
+docker push $1/compendium:${tag}
+docker push $1/compendium:latest
