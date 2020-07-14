dccd8de2053bba2f6caf9c367350ffc942a5267c
Ana Gómez <ana.gomez@47deg.com>
2020-07-14 17:46:06 +0200

    Include new migration to update idl types in compendium databases

--- src/main/resources/db/migration/metadata/V1__add_metaprotocols_table.sql
+++ src/main/resources/db/migration/metadata/V1__add_metaprotocols_table.sql
@@ -1,4 +1,4 @@
-CREATE TYPE idl AS ENUM ('avro', 'protobuf', 'mu', 'openapiyaml', 'openapijson', 'scala');
+CREATE TYPE idl AS ENUM ('avro', 'protobuf', 'mu', 'openapi', 'scala');
 
 CREATE TABLE metaprotocols (
     id CHARACTER VARYING(255) PRIMARY KEY,
---
+++ src/main/resources/db/migration/metadata/V2__update_idl_types.sql
@@ -0,0 +1,12 @@
+ALTER TYPE idl ADD VALUE 'openapiyaml';
+ALTER TYPE idl ADD VALUE 'openapijson';
+
+UPDATE metaprotocols SET idl_name = 'openapiyaml' WHERE idl_name = 'openapi'
+
+ALTER TYPE idl RENAME TO idl_old;
+
+CREATE TYPE idl as ENUM('avro', 'protobuf', 'mu', 'openapiyaml', 'openapijson', 'scala');
+
+ALTER TABLE metaprotocols ALTER COLUMN idl_name TYPE idl USING idl_name::text::idl;
+
+DROP TYPE idl_old;
