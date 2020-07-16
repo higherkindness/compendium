528b2acab1f7e8a0c8876d55b75ba45f2536bab7
Ana Gómez González <ana.gomez@47deg.com>
2020-07-16 11:09:52 +0200

    Simplify migration file adding new idl type
    Co-authored-by: Fede Fernández <720923+fedefernandez@users.noreply.github.com>

--- src/main/resources/db/migration/metadata/V2__update_idl_types.sql
+++ src/main/resources/db/migration/metadata/V2__update_idl_types.sql
@@ -1,7 +1,1 @@
-ALTER TYPE idl RENAME TO idl_old;
-
-CREATE TYPE idl as ENUM('avro', 'protobuf', 'mu', 'openapi', 'openapijson', 'scala');
-
-ALTER TABLE metaprotocols ALTER COLUMN idl_name TYPE idl USING idl_name::text::idl;
-
-DROP TYPE idl_old;
+ALTER TYPE idl ADD VALUE 'openapijson';
